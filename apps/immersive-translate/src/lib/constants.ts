bfbf63a549fb586273276acaa460d5d2cf950dcf
jongminchung <chungjm0711@gmail.com>
2026-07-20 17:57:09 +0900

    feat: remove immersive translate

--- apps/immersive-translate/src/lib/constants.ts
+++
@@ -1,8 +0,0 @@
-export const APP_NAME = "Tobi Immersive Translate";
-export const STORAGE_PREFIX = "TS";
-export const WORKSPACE_STORAGE_KEY = "workspace";
-export const DRIVE_SYNC_STORAGE_KEY = "drive-sync";
-export const LOCAL_TRANSLATION_STORAGE_KEY = "localTranslation";
-export const DEFAULT_DRIVE_FILE_NAME = "immersive-translate-workspace.json";
-
-export const COLLECTION_COLORS = ["#f6bd60", "#f28482", "#84a59d", "#90be6d", "#b8c0ff", "#cdb4db"];
